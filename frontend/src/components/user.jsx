d20028ec87c4e6ad5715b95cae5e5cf1ae067b73
zcbe <151560157+zcbe@users.noreply.github.com>
2024-05-08 00:04:23 +0200

    Gestion formulaire d'édition du pseudo

--- frontend/src/components/user.jsx
+++ frontend/src/components/user.jsx
@@ -1,15 +1,44 @@
 import React, { useState } from "react";
-import { useSelector } from "react-redux"; // Importe la fonction useSelector de Redux pour accéder à l'état global
+import { useDispatch, useSelector } from "react-redux"; // Importe la fonction useSelector de Redux pour accéder à l'état global
 import '../sass/components/_UserProfile.scss';
 
 function User() { // Définit le composant fonctionnel User
+    const token = useSelector((state) => state.auth.token);
     // Utilise useSelector pour extraire le prénom de l'utilisateur à partir de l'état global
     const firstname = useSelector((state) => state.auth.user.firstname);
     // Utilise useSelector pour extraire le nom de famille de l'utilisateur à partir de l'état global
     const lastname = useSelector((state) => state.auth.user.lastname);
     const username = useSelector((state) => state.auth.user.username);
     const [display, setDisplay] = useState(true); // État local pour contrôler l'affichage du profil ou du formulaire d'édition
-    const [editUsername, setEditUsername] = useState(''); // État local pour stocker la valeur en cours d'édition du nom d'utilisateur
+    const [userName, setUserName] = useState('');  // État local pour stocker la valeur en cours d'édition du nom d'utilisateur
+    const dispatch = useDispatch(); // Crée une fonction dispatch pour envoyer des actions à Redux
+
+    const handleSubmitUsername = async (event) => { // Fonction pour gérer la soumission du nom d'utilisateur modifié
+        event.preventDefault();  // Empêche le comportement par défaut du formulaire de recharge de la page
+        try {
+            const response = await fetch('http://localhost:3001/api/v1/user/profile', { // Effectue une requête HTTP PUT pour mettre à jour le profil utilisateur
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`, // Inclut le token d'authentification dans l'en-tête de la requête
+                },
+                body: JSON.stringify({userName}), // Corps de la requête contenant le nouveau nom d'utilisateur
+            });
+            if (response.ok) { // Vérifie si la réponse de la requête est réussie
+                const data = await response.json(); // Récupère les données renvoyées par la requête
+                console.log(data); // Affiche les données dans la console
+                dispatch({ // Dispatch une action Redux pour mettre à jour le nom d'utilisateur dans le store global
+                    type: 'EDIT_USERNAME',
+                    payload: {username: data.body.userName}, // Payload contenant le nouveau nom d'utilisateur
+                });
+            } else {
+                console.log("Invalid Fields") // Affiche un message d'erreur si les champs sont invalides
+            }
+        } catch (error) {
+            console.error(error); // Gère les erreurs potentielles lors de l'exécution de la requête
+        }
+    }
+
 
     return (
         <div className="header">
@@ -31,7 +60,7 @@ function User() { // Définit le composant fonctionnel User
                                 type="text"
                                 id="username"
                                 defaultValue={username} 
-                                onChange={(event) => setEditUsername(event.target.value)} // Met à jour l'état local editUsername lorsqu'il y a un changement dans le champ de saisie
+                                onChange={(event) => setUserName(event.target.value)} // Met à jour l'état local userName lorsqu'il y a un changement dans le champ de saisie
                             />
                         </div>
                         <div className="edit-input">
@@ -54,7 +83,7 @@ function User() { // Définit le composant fonctionnel User
                         </div>
                         <div className="buttons">
                             <button className="edit-username-button">Save</button>
-                            <button className="edit-username-button" onClick={() => setDisplay(!display)}>Cancel</button>
+                            <button className="edit-username-button" onClick={handleSubmitUsername}>Save</button>
                         </div>
                     </form>
                 </div>
@@ -64,3 +93,4 @@ function User() { // Définit le composant fonctionnel User
 }
 
 export default User
+
--- frontend/src/redux/reducers/auth.reducer.jsx
+++ frontend/src/redux/reducers/auth.reducer.jsx
@@ -2,57 +2,74 @@
 import { initialState } from "../store";
 
 // Importe les types d'actions depuis un fichier qui contient les définitions des types d'actions utilisés dans l'application
-import { LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT, GET_USERPROFILE } from "../actions/type.actions";
+import {
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+  GET_USERPROFILE,
+  EDIT_USERNAME,
+} from "../actions/type.actions";
 
 // Définit un réducteur (authReducer) responsable de gérer l'état lié à l'authentification
 export const authReducer = (state = initialState, action) => {
-    // Utilise une instruction switch pour examiner le type d'action
-    switch (action.type) {
-        // Si le type d'action correspond à LOGIN_SUCCESS, le code suivant est exécuté
-        case LOGIN_SUCCESS:
-            // Retourne un nouvel objet d'état avec les propriétés mises à jour en fonction de l'action LOGIN_SUCCESS
-            return {
-                ...state,
-                status: "CONNECTED",
-                isConnected: true,
-                token: action.payload,
-                error: null
-            };
+  // Utilise une instruction switch pour examiner le type d'action
+  switch (action.type) {
+    // Si le type d'action correspond à LOGIN_SUCCESS, le code suivant est exécuté
+    case LOGIN_SUCCESS:
+      // Retourne un nouvel objet d'état avec les propriétés mises à jour en fonction de l'action LOGIN_SUCCESS
+      return {
+        ...state,
+        status: "CONNECTED",
+        isConnected: true,
+        token: action.payload,
+        error: null,
+      };
 
-        // Si le type d'action correspond à LOGIN_FAIL, le code suivant est exécuté
-        case GET_USERPROFILE: {
-            // Retourne un nouvel objet d'état avec les informations du profil utilisateur mises à jour
-            return {
-                ...state,
-                status: "SUCCEEDED",
-                user: {
-                    ...state.user,
-                    firstname: action.payload.firstname,
-                    lastname: action.payload.lastname,
-                    username: action.payload.username
-                }
-            }
-        }
-
-
-        case LOGIN_FAIL: {
-            // Retourne un nouvel objet d'état avec les propriétés mises à jour en fonction de l'action LOGIN_FAIL
-            return {
-                ...state,
-                status: "FAILED",
-                isConnected: false,
-                error: action.payload
-            };
-        }
-
-        // Si le type d'action correspond à LOGOUT, le code suivant est exécuté
-        case LOGOUT: {
-            // Retourne l'état initial de l'application, réinitialisant ainsi complètement l'état de l'authentification
-            return initialState;
-        }
-
-        // Si le type d'action ne correspond à aucun des cas précédents, retourne simplement l'état sans effectuer de modification
-        default:
-            return state;
+    // Si le type d'action correspond à LOGIN_FAIL, le code suivant est exécuté
+    case GET_USERPROFILE: {
+      // Retourne un nouvel objet d'état avec les informations du profil utilisateur mises à jour
+      return {
+        ...state,
+        status: "SUCCEEDED",
+        user: {
+          ...state.user,
+          firstname: action.payload.firstname,
+          lastname: action.payload.lastname,
+          username: action.payload.username,
+        },
+      };
     }
+
+    case EDIT_USERNAME: // Action type EDIT_USERNAME, généralement déclenchée lorsqu'un utilisateur modifie son nom d'utilisateur
+      return {
+        // Retourne un nouvel état global
+        ...state, // Déstructure l'état actuel pour garantir l'immutabilité
+        status: "MODIFIED", // Met à jour le statut pour indiquer que le nom d'utilisateur a été modifié
+        user: {
+          // Met à jour les informations de l'utilisateur
+          ...state.user, // Déstructure les informations utilisateur actuelles
+          username: action.payload.username, // Met à jour le nom d'utilisateur avec la nouvelle valeur provenant de l'action payload
+        },
+      };
+
+    case LOGIN_FAIL: {
+      // Retourne un nouvel objet d'état avec les propriétés mises à jour en fonction de l'action LOGIN_FAIL
+      return {
+        ...state,
+        status: "FAILED",
+        isConnected: false,
+        error: action.payload,
+      };
+    }
+
+    // Si le type d'action correspond à LOGOUT, le code suivant est exécuté
+    case LOGOUT: {
+      // Retourne l'état initial de l'application, réinitialisant ainsi complètement l'état de l'authentification
+      return initialState;
+    }
+
+    // Si le type d'action ne correspond à aucun des cas précédents, retourne simplement l'état sans effectuer de modification
+    default:
+      return state;
+  }
 };
